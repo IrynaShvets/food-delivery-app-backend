0155bbd52011a51be1e2082316c2173f1704683d
IrynaShvets <irinashwets@ukr.net>
2022-06-30 04:34:30 +0300

    fix

--- bin/server.js
+++ bin/server.js
@@ -2,13 +2,12 @@ const mongoose = require("mongoose");
 
 const app = require("../app");
 
-const { DB_HOST, PORT = 5555 } = process.env;
+const { DB_HOST, PORT } = process.env;
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(PORT);
-    /* app.listen(PORT || 5555); */
+    app.listen(PORT || 5555);
     console.log("Database connection successful");
   })
   .catch((error) => {
